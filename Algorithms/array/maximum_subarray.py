5a1dfe37c4ad415a50bf275f9670f4788cec8d41
Naveen Basavaraj <naveen.b.workmail@gmail.com>
2023-10-20 19:04:19 +0530

    level 1

--- Algorithms/array/maximum_subarray.py
+++ Algorithms/array/maximum_subarray.py
@@ -10,23 +10,3 @@ class MaxSubarray:
             currSum +=  num
             maxSum = max(maxSum, currSum)
         return maxSum
-    
-    # def maxSumSubArray(self, nums):
-    #     currSum = nums[0]
-    #     maxSum = 0
-    #     for r in range(1, len(nums)):
-    #         if currSum < 0:
-    #             currSum = 0
-    #         currSum +=  nums[r]
-    #         maxSum = max(maxSum, currSum)
-    #     return maxSum
-    
-    def maxProductSubArray(self, nums):
-        res = max(nums)
-        curMin, curMax = 1,1
-        for n in nums:
-            tmp = curMax * n
-            curMax = max(n*curMax, n* curMin, n)
-            curMin = min(tmp,  n*curMin, n)
-            res = max(res, curMax)
-        return res
