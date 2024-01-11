2890e5a6605d692cd0de3d27c6adc497557909cc
Naveen Basavaraj <61464089+NaveenBasavaraj@users.noreply.github.com>
2024-01-11 23:36:08 +0530

    authentication

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # django_projects
- django_projects
+
+django_projects
+
+virtualenv django_common_env
+django_common_env\scripts\activate
+
+pip install django
+pip install pygments
--- emp_management_system/poll/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from poll.views import *
-
-app_name = "poll"
-urlpatterns = [
-    path("", index, name="index"),
-    path("<int:id>/detail/", details, name="details"),
-    path("<int:id>/", poll_page, name="poll_page"),
-]
