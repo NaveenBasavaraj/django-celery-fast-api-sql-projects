a5cda8870eed2ea32b861ae031621442cd090cf5
Naveen Basavaraj <61464089+NaveenBasavaraj@users.noreply.github.com>
2024-02-17 08:07:24 +0530

    a

--- dj_books_cbv/books/urls.py
+++ dj_books_cbv/books/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from books.views import HomeView
 
-app_name = 'books'
+app_name = "books"
 urlpatterns = [
-    path('', HomeView.as_view(), name="home")
+    path("", HomeView.as_view(), name="home"),
 ]
