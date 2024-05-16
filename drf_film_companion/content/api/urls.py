4999cdffae78fe38c17ae0c88465c5159700a09a
Naveen Basavaraj <61464089+NaveenBasavaraj@users.noreply.github.com>
2024-05-16 19:11:30 +0530

    changed urls, added class based view file

--- drf_film_companion/content/api/urls.py
+++ drf_film_companion/content/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from content.api.views_json import movie_list, movie_details
-from content.api.views import movies_serialized_list, movie_serialized_details
+from drf_film_companion.content.api.views_function import movies_serialized_list, movie_serialized_details
 
 urlpatterns = [
-    path('json/', movie_list, name='movie-list'),
-    path('json/<int:pk>', movie_details, name='movie-details'),
-    path('list/', movies_serialized_list, name='movies_serialized_list'),
-    path('list/<int:pk>', movie_serialized_details, name='movie_serialized_details'),
+    path('json/list', movie_list, name='movie-list'),
+    path('json/list/<int:pk>', movie_details, name='movie-details'),
+    path('func/list', movies_serialized_list, name='movies_serialized_list'),
+    path('func/list/<int:pk>', movie_serialized_details, name='movie_serialized_details'),
 ]
---
+++ drf_film_companion/content/api/views_class.py
@@ -0,0 +1,8 @@
+from content.models import Movie
+from content.api.serializers import MovieSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+
+class MovieListAV():
+    pass
